ff2807f4a0204259ec93adf2d3eb41dbad879142
andiricum2 <andiricum2@gmail.com>
2023-03-12 11:54:51 +0100

    Initial commit

---
+++ src/main/java/com/orioland/ultimateol/UltimateOrioLand.java
@@ -0,0 +1,14 @@
+package com.orioland.ultimateol;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UltimateOrioLand implements ModInitializer {
+	public static final String ModID = "ultimateol";
+	public static final Logger LOGGER = LoggerFactory.getLogger(ModID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/com/orioland/ultimateol/UltimateOrioLandClient.java
@@ -0,0 +1,10 @@
+package com.orioland.ultimateol;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UltimateOrioLandClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
